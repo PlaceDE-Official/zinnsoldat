b13e351459aab214f532b4cb45ac43985dce6b00
Luca <root@gituser.de>
2023-07-21 13:57:58 +0200

    Update placebot.user.js

--- output/placebot.user.js
+++ output/placebot.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  Einer von uns!
 // @author       placeDE Devs
-// @match        https://www.reddit.com/r/place/*
+// @match        https://*.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/PlaceDE-Official/zinnsoldat/raw/main/output/placebot.user.js
 // @downloadURL  https://github.com/PlaceDE-Official/zinnsoldat/raw/main/output/placebot.user.js
