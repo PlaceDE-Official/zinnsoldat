8492341ce6a9d2e949658d3e6720d47d5b8a31a0
Logan <hello@loganthompson.de>
2023-07-23 03:20:33 +0200

    Remove Userscript

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         r/placeDE Zinnsoldat
-// @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  Einer von uns!
-// @author       placeDE Devs
-// @match        https://*.reddit.com/r/place/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/PlaceDE-Official/zinnsoldat/raw/main/output/placebot.user.js
-// @downloadURL  https://github.com/PlaceDE-Official/zinnsoldat/raw/main/output/placebot.user.js
-// ==/UserScript==
 (async () => {
     // Check for correct page
     if (!window.location.href.startsWith('https://www.reddit.com/r/place/') && !window.location.href.startsWith('https://new.reddit.com/r/place/')) {
